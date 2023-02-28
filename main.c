b82afec187b7be85f9e354057411f66e068eac0d
kacperfin <kacper.aleksander.fin@gmail.com>
2023-02-28 19:43:49 +0100

    Basic error handling and opening a file

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-	bool debug_mode = false;
+	bool debug_mode;
 
 	if(argc<=1)
 	{
@@ -14,9 +14,42 @@ int main(int argc, char *argv[])
 	}
 
 	if(strcmp("-d", argv[1])==0) debug_mode = true;
+	else if(strcmp("-f", argv[1])==0) debug_mode = false;
+	else
+	{
+		printf("Blednie podano flage. Podaj jedna z dwoch flag: \"\"-f\"\" lub \"\"-d\"\"\n");
+		return -2;
+	}
 
+	/*
+	Sprawdzanie poprawnosci dzialania przypisywania flag
+	
 	if(debug_mode==true) printf("DEBUG: %s\n", argv[1]);
-	else printf("%s\n", argv[1]);
+	else printf("NORMAL: %s\n", argv[1]);
+	*/
+
+	if(debug_mode==true)
+	{
+		;
+	}
+	else
+	{
+		if(argc<=3)
+		{
+			printf("Nie podano pliku wejsciowego i wyjsciowego. Przyklad: ./a.out -f PLIK_WEJSCIOWY PLIK_WYJSCIOWY\n");
+			return -3;
+		}
+
+		FILE *fp;
+		fp = fopen(argv[2], "r");
+		if(fp==NULL)
+		{
+			printf("Blad przy otwieraniu pliku.\n");
+			return -4;
+		}
+
+
+	}
 
 	return 0;
 }
