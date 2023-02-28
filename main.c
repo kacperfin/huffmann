7182493c26c85501137447f7ea90c5e4812b0d50
kacperfin <kacper.aleksander.fin@gmail.com>
2023-02-28 19:12:36 +0100

    Debug mode - if statements

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	printf("Hello\n");
+	bool debug_mode = false;
+
+	if(argc<=1)
+	{
+		printf("Brak podanych argumentow. Podaj jedna z dwoch flag: \"\"-f\"\" lub \"\"-d\"\"\n");
+		return -1;
+	}
+
+	if(strcmp("-d", argv[1])==0) debug_mode = true;
+
+	if(debug_mode==true) printf("DEBUG: %s\n", argv[1]);
+	else printf("%s\n", argv[1]);
+
+	return 0;
 }
